0805bdc44d2ed0ea91da20bc5b47556d55c5c540
Data Viz <jlmobile710@gmail.com>
2019-01-20 15:55:42 +0000

    save & html

--- d3/svg-editor/assets/js/script.js
+++ d3/svg-editor/assets/js/script.js
@@ -2,10 +2,7 @@ var width = Math.min(1200, document.getElementById('main_content').clientWidth);
 height = 800;
 var currentTool = "mouse"; // Selected tool in toolbox
 var index = 0;
-var svg = d3.select("#svg_wrapper").append("svg")
-    .attr('id','svg')
-    .attr("width", width)
-    .attr("height", height)
+var svg = d3.select("svg")
     .on("mousedown", mousedown)
     .on("mouseup", mouseup);
 
@@ -20,77 +17,6 @@ var roomStart = svgWidth / 4 + padding;
 var index = 4;
 var squareWidth = 30;
 var roomWidth = 360, roomHeight = 700;
-drawBasicFloor();
-function drawBasicFloor(){
-    svg.append('rect').attr('x', svgWidth/4).attr('class','element').attr('y',50).attr('width',roomWidth).attr('height',700).attr('fill',"red").attr('id','rect_3');
-    svg.append('circle').attr('cx', svgWidth/4).attr('class','element').attr('cy',svgHeight / 2).attr('r',svgHeight/6).attr('fill',"red").attr('id','circle_2');    
-    svg.append('circle').attr('cx', svgWidth/4).attr('class','element').attr('cy',svgHeight / 2).attr('r',30).attr('fill',"steelblue").attr('stroke','black').attr('id','circle_1');    
-    drawRect(roomStart,margin.top+padding,squareWidth,squareWidth);
-    drawRect(roomStart + padding + squareWidth,margin.top+padding,squareWidth,squareWidth);
-    drawRect(roomStart + 2 * (padding + squareWidth),margin.top+padding,squareWidth,squareWidth);
-    drawRect(roomStart - 10, margin.top+padding+40, squareWidth * 6, 10);
-    drawRect(roomStart,margin.top+padding+60,squareWidth,squareWidth);
-    drawRect(roomStart + padding + squareWidth,margin.top+padding+60,squareWidth,squareWidth);
-    drawRect(roomStart + 2 * (padding + squareWidth),margin.top+padding+60,squareWidth,squareWidth);
-
-    drawRect(roomStart,margin.top+padding + squareWidth * 3 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + padding + squareWidth,margin.top+padding + squareWidth * 3 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + 2 * (padding + squareWidth),margin.top+padding + squareWidth * 3 + padding,squareWidth,squareWidth);
-    drawRect(roomStart - 10, margin.top+padding+40 + squareWidth * 3 + padding, squareWidth * 6, 10);
-    drawRect(roomStart,margin.top+padding+60 + squareWidth * 3 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + padding + squareWidth,margin.top+padding+60 + squareWidth * 3 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + 2 * (padding + squareWidth),margin.top+padding+60 + squareWidth * 3 + padding,squareWidth,squareWidth);
-
-    drawRect(roomStart,margin.top+padding + squareWidth * 14 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + padding + squareWidth,margin.top+padding + squareWidth * 14 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + 2 * (padding + squareWidth),margin.top+padding + squareWidth * 14 + padding,squareWidth,squareWidth);
-    drawRect(roomStart - 10, margin.top+padding+40 + squareWidth * 14 + padding, squareWidth * 6, 10);
-    drawRect(roomStart,margin.top+padding+60 + squareWidth * 14 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + padding + squareWidth,margin.top+padding+60 + squareWidth * 14 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + 2 * (padding + squareWidth),margin.top+padding+60 + squareWidth * 14 + padding,squareWidth,squareWidth);
-
-
-    drawRect(roomStart,margin.top+padding + squareWidth * 18 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + padding + squareWidth,margin.top+padding + squareWidth * 18 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + 2 * (padding + squareWidth),margin.top+padding + squareWidth * 18 + padding,squareWidth,squareWidth);
-    drawRect(roomStart - 10, margin.top+padding+40 + squareWidth * 18 + padding, squareWidth * 6, 10);
-    drawRect(roomStart,margin.top+padding+60 + squareWidth * 18 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + padding + squareWidth,margin.top+padding+60 + squareWidth * 18 + padding,squareWidth,squareWidth);
-    drawRect(roomStart + 2 * (padding + squareWidth),margin.top+padding+60 + squareWidth * 18 + padding,squareWidth,squareWidth);
-
-    drawRect(roomStart + svgWidth / 5 * 2, margin.top+padding, 25, 40);
-    drawRect(roomStart + svgWidth / 5 * 2, margin.top+padding + 50, 25, 40);
-    drawRect(roomStart + svgWidth / 5 * 2, margin.top+padding + 2 * 50, 25, 40);
-    drawRect(roomStart + svgWidth / 5 * 2, margin.top+padding + 3* 50, 25, 40);
-    drawRect(roomStart + svgWidth / 5 * 2, margin.top+padding + 9 * 50, 25, 40);
-    drawRect(roomStart + svgWidth / 5 * 2, margin.top+padding + 10 * 50, 25, 40);
-    drawRect(roomStart + svgWidth / 5 * 2, margin.top+padding + 11* 50, 25, 40);
-    drawRect(roomStart + svgWidth / 5 * 2, margin.top+padding + 12 * 50, 25, 40);
-
-    drawRect(roomStart + roomWidth - 80, margin.top+ padding, 10, 200);
-    drawRect(roomStart + roomWidth - 120, margin.top+ padding + 4 * 50 + 25, 25, 200);
-    drawRect(roomStart + roomWidth - 80, margin.top+ padding + 9 * 50, 10, 200);
-
-    drawRect(roomStart - 80, svgHeight / 2 - svgHeight / 6 + 50, squareWidth, squareWidth);
-    drawRect(roomStart , svgHeight / 2 - svgHeight / 6 + 50, squareWidth, squareWidth);
-    drawRect(roomStart - 80, svgHeight / 2 - svgHeight / 6 + 190, squareWidth, squareWidth);
-    drawRect(roomStart , svgHeight / 2 - svgHeight / 6 + 190, squareWidth, squareWidth);
-
-    drawRect(roomStart - 130, svgHeight / 2 - svgHeight / 6 + 90, squareWidth, squareWidth);
-    drawRect(roomStart - 130, svgHeight / 2 - svgHeight / 6 + 150, squareWidth, squareWidth);
-
-    drawRect(roomStart + svgWidth / 5 * 2 - 40, svgHeight / 2 - svgHeight / 6 + 50, squareWidth, squareWidth);
-    drawRect(roomStart + svgWidth / 5 * 2 - 40, svgHeight / 2 - svgHeight / 6 + 2 * 50, squareWidth, squareWidth);
-    drawRect(roomStart + svgWidth / 5 * 2 - 40, svgHeight / 2 - svgHeight / 6 + 3 * 50, squareWidth, squareWidth);
-    drawRect(roomStart + svgWidth / 5 * 2 - 40, svgHeight / 2 - svgHeight / 6 + 4 * 50, squareWidth, squareWidth);    
-
-    drawRect(roomStart + roomWidth - 30, margin.top-10, 20, roomHeight+20, "white");
-    drawRect(roomStart + roomWidth - 10, margin.top-10, 40, roomHeight+20, "lightgreen");
-    drawRect(roomStart + roomWidth + 30, svgHeight / 2 - 100, 30, 200, "purple");
-    drawRect(roomStart + roomWidth + 30, margin.top + 30, 20, 50, "white");
-    drawRect(roomStart + roomWidth + 30, margin.top + 600, 20, 50, "white");
-}
-
 
 var dragger = d3.drag()
     .on('drag', handleDrag)
@@ -98,12 +24,14 @@ var dragger = d3.drag()
         dragging = false;
     });
 function mousedown() {
+    console.log('mousedown');
     index++;
     //if currentTool == rect
     var m = d3.mouse(this);
-    
+    console.log(svg);
     if(currentTool=="rect"){
-        rect = svg.append("rect")
+    console.log('here');
+        rect = d3.select('svg').append("rect")
         .attr("x", m[0])
         .attr("y", m[1])
         .attr('class','element')
@@ -111,11 +39,11 @@ function mousedown() {
         .attr("width", 0)
         .attr('fill','red')
         .attr('stroke','black')
-        .attr('id',"rect_"+index);
+        .attr('id',"rect_"+Math.floor((Math.random() * 10000) + 1));
 
-        svg.on("mousemove", mousemove);
+        d3.select('svg').on("mousemove", mousemove);
     }else if(currentTool=="ellipse"){
-        ellipse = svg.append("ellipse")
+        ellipse = d3.select('svg').append("ellipse")
         .attr("cx", m[0])
         .attr("cy", m[1])
         .attr('sx',m[0])
@@ -127,9 +55,9 @@ function mousedown() {
         .attr("fill","red")
         .attr("stroke","black");
 
-        svg.on("mousemove", mousemove);
+        d3.select('svg').on("mousemove", mousemove);
     }else if(currentTool=="line"){
-        line = svg.append("line")
+        line = d3.select('svg').append("line")
         .attr("x1", m[0]).attr('class','element')
         .attr("y1", m[1])        
         .attr('id',"line_"+index)
@@ -141,15 +69,13 @@ function mousedown() {
             if(currentTool=="mouse"){
                 $("#delete-btn").removeClass("disabled");
                 $("#selected_id").val($(this).attr('id'));
+                $('#element_id').val($(this).attr('id'));
             }    
         });
         // $("#delete-btn").addClass("disabled");
     }
 }
-function drawRect(x,y,width, height,backcolor="red",strokeStyle){
-    index++;
-    svg.append('rect').attr('x',x).attr('y',y).attr('width',width).attr('class','element').attr('height',height).attr('id','rect_'+index).attr('fill',backcolor).attr('stroke','black');
-}
+
 function mousemove(d) {
     var m = d3.mouse(this);
     if(currentTool=="rect"){
@@ -202,11 +128,11 @@ function getRandomColor() {
 
 function mouseup() {
     if(currentTool=="rect"){
-        svg.on("mousemove", null);
+        d3.select('svg').on("mousemove", null);
     }else if(currentTool=="ellipse"){
-        svg.on("mousemove", null);
+        d3.select('svg').on("mousemove", null);
     }else if(currentTool =="line"){
-        svg.on("mousemove", null);
+        d3.select('svg').on("mousemove", null);
     }else if(currentTool=="polygon"){
         if(dragging) return;
         drawing = true;
@@ -297,6 +223,7 @@ $(document).ready(function () {
 
     $("#delete-btn").click(function(){
         var s = "#"+$("#selected_id").val();
+        console.log($(s));        
         if(s!=="#"){
             $(s).remove();            
             $(this).addClass('disabled');
@@ -310,27 +237,36 @@ $(document).ready(function () {
         var name = $("#name").val();
         console.log('add');
         $.ajax({
-            type:'POST',
-            data:{"content":"testabc111","q":"saveBlob"},
+            method:'POST',
+            data:{"content":s,"q":"saveBlob"},
             dataType:"json",
             url:'https://reg6.meetmatch.biz/api.php',
             success:function(response) {
-                $.notify({message: 'Success to save' },{type: 'success'});        
+                
             },error:function(error){
-                console.log(error);
+                if(error.status == 200){
+                    $.notify({message: 'Success to save' },{type: 'success',delay: 1000,timer: 1000});
+                }                
             }
         });
+    })
 
-        // $.ajax({
-        //     type:'GET',
-        //     data:{"q":"getBlob"},
-        //     dataType:"json",
-        //     url:'https://reg6.meetmatch.biz/api.php',
-        //     success:function(data) {
-        //         console.log(data);
-        //         $.notify({message: 'Success to Load' },{type: 'success'});        
-        //     },
-        // })
+    $("#load-btn").click(function(e){
+        $.ajax({
+            type:'GET',
+            data:{"q":"getBlob"},
+            dataType:"json",
+            url:'https://reg6.meetmatch.biz/api.php',
+            success:function(data) {                
+            },error:function(error){
+                console.log(error);
+                $.notify({message: 'Success to Load' },{type: 'success',delay: 1000,timer: 1000});                
+                $("#svg_wrapper").html(error.responseText);
+                d3.select('svg')    
+                    .on("mousedown", mousedown)
+                    .on("mouseup", mouseup);
+            }
+        })
     })
 });
 
@@ -340,3 +276,8 @@ d3.selectAll('.element').on("mousedown", function(){
         $("#selected_id").val($(this).attr('id'));         
     }    
 });
+
+$('#element_id').on('input',function(){
+    var s = "#" + $("#selected_id").val();
+    d3.select(s).attr('id',$(this).val());
+})
