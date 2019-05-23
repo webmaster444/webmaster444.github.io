2fecf1fe1c5df2f388f0d8a01f742c750575c808
Data Viz <jlmobile710@gmail.com>
2019-05-23 01:23:43 -0700

    Sidebar and chart integration

--- d3/Ralph-Welborn/portfolio-chart/assets/js/script.js
+++ d3/Ralph-Welborn/portfolio-chart/assets/js/script.js
@@ -8,6 +8,7 @@ var margin = {
     bottom: 50
 };
 
+var optionData,csvData;
 var width = wrapperWidth - margin.left - margin.right;
 var height = wrapperHeight - margin.top - margin.bottom;
 var svg = d3.select("#chart_wrapper").append('svg').attr("width", wrapperWidth).attr("height", wrapperHeight).append("g").attr("transform", 'translate('+margin.left+',' + margin.top + ')')
@@ -21,21 +22,58 @@ var treemap = d3.treemap()
     .padding(1)
     .round(true);
 
-function sumByValue(d) {
-    return d.value;
+function sumByCost(d) {
+    return d['Cost'];
+}
+
+function sumByTime(d) {
+    return d['Time'];
 }
 
 var x = d3.scaleLinear().range([0, width]);
 var x_axis = d3.axisBottom(x);
-
 d3.csv("assets/changed-data.csv").then(function(data) {
+d3.json("assets/filter-data.json").then(function(option){
+
+    csvData = data;
+    optionData = option;
+
+    var viewby = $("input[name='viewby']:checked").val();
+
+
+    viewBySelectUpdate();
+
+    var filters = option.filters.map(function(d){return d.name});
+
+    filters.forEach(function(opt){
+        var html = '<option value="'+opt+'">'+opt+'</option>';
+        $("#filterbySelect").append(html);
+    })
+
+
+    getSubFilters();
+    selectOne("filter");
+
+    getColorGroups();
+    var newData = getData(viewby, $("#viewBySelect").val(), data);
+
+    getTags(newData);
+    drawChart(viewby, 'Cost', $("#viewBySelect").val(), newData);
+})
+})
+
+function getData(viewby, viewValue, data){
+    return data.filter(function(d){return d[viewby] == viewValue});
+}
+
+function drawChart(viewby, sizeby, viewValue, data){
     var businessUnitData = d3.nest()
         .key(function(d) {
-            return d['Business Unit'];
+            return d["Department"];
         })
         .rollup(function(v) {
             return d3.sum(v, function(d) {
-                return d['Cost'];
+                return d[sizeby];
             });
         })
         .entries(data);
@@ -53,6 +91,8 @@ d3.csv("assets/changed-data.csv").then(function(data) {
         u.startX = x;
     })
 
+    svg.html('');
+
     var units_g = svg.selectAll('.unit_g').data(businessUnitData).enter().append('g').attr('class', (d, i) => 'unit_g unit_' + i).attr('transform', (d) => "translate(" + x(d.startX) + ",0)");
     units_g.append('rect').attr('x', 0).attr("width", (d) => x(d.value)).attr("y", -50).attr("height", 50)
     // .attr('fill', (d)=>color(d.key))
@@ -61,33 +101,27 @@ d3.csv("assets/changed-data.csv").then(function(data) {
     units_g.append('text').attr("x", 10).attr('y', -20).text((d) => d.key).attr("fill", 'white');
 
     businessUnitData.forEach(function(d, i) {
-        console.log(d);
+        
         var newData = data.filter(function(datum) {
-            return datum['Business Unit'] == d.key
+            return datum['Department'] == d.key
         });
 
         var departmentData = d3.nest()
             .key(function(d) {
                 return d['Department'];
             })
-            .rollup(function(v) {
-                return d3.sum(v, function(d) {
-                    return d['Cost'];
-                });
-            })
-            .entries(newData);
+            .entries(newData);        
 
         var newObj = new Object;
         newObj['name'] = d.key;
-        newObj['children'] = departmentData;
-
-        console.log(newObj);
+        newObj['children'] = departmentData[0].values;
+        
         var root = d3.hierarchy(newObj)
             .eachBefore(function(d) {
                 d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.name;
             })
-            .sum(sumByValue)
-            .sort(function(a, b) {
+            .sum(sumByCost)
+            .sort(function(a, b) {                
                 return b.height - a.height || b.value - a.value;
             });
 
@@ -133,13 +167,107 @@ d3.csv("assets/changed-data.csv").then(function(data) {
             .attr("x", (d) => (d.x1 - d.x0) / 2)
             .attr("y", (d) => (d.y1 - d.y0) / 2)
             .attr('text-anchor', 'middle')
-            .text(function(d) {
-                return d.data.key ;
+            .text(function(d) {                
+                return d.data.Capability ;
             });
 
         cell.append("title")
             .text(function(d) {
-                return d.data.key + "\n" + format(d.value);
+                return d.data.Capability + "\n" + format(d.value);
             });
+
     })
-})
+}
+
+function selectOne(name){
+    $('input[name="'+name + '"]').first().prop('checked',true);
+}
+
+function getTags(data){
+    var tags = [];
+
+    var tag = $("#tagsSelect").val();
+
+    data.map(function(d){return d[tag]}).forEach(function(d){
+        if(!tags.includes(d)){
+            tags.push(d);
+        }
+    });
+
+    $("#tags_section").html("");
+    tags.forEach(function(t){
+        var html = '<span class="tag">'+t+'</span>';
+        $("#tags_section").append(html);
+    })
+}
+
+function getColorGroups(){    
+    var filterV = $("#filterbySelect").val();
+    var subfilter = $("input[name='filter']:checked").val();
+        
+    var colorGroups = [];
+    optionData.filters.forEach(function(opt){
+        if(opt.name == filterV){
+            opt.children.forEach(function(o){
+                if(o.name == subfilter){
+                    colorGroups = o.children;
+                }
+            })
+        }
+    });
+    
+    return colorGroups;
+}
+
+$(document).on('change','input[name="viewby"]',function(){
+    viewBySelectUpdate();
+    var newData = getData($("input[name='viewby']:checked").val(), $("#viewBySelect").val(), csvData);
+    getTags(newData);
+});
+
+$(document).on('change','input[name="filter"]',function(){
+    getColorGroups();
+});
+
+$(document).on('change','#viewBySelect',function(){
+    var newData = getData($("input[name='viewby']:checked").val(), $("#viewBySelect").val(), csvData);
+    drawChart($("input[name='viewby']:checked").val(), 'Cost', $("#viewBySelect").val(), newData);    
+    getTags(newData);
+});
+
+$(document).on('change','#filterbySelect',function(){
+    getSubFilters();
+    selectOne("filter");
+    getColorGroups();
+});
+
+function getSubFilters(){
+    var selectedFilter = $("#filterbySelect").val();
+
+    $("#filter_radio_section").html("");
+    optionData.filters.forEach(function(opt){
+        if(opt.name == selectedFilter){
+            opt.children.map(function(o){return o.name}).forEach(function(n){
+                var html = '<input type="radio" name="filter" value="'+n +'"/>' + n;
+                $("#filter_radio_section").append(html);
+            })
+        }        
+    })
+}
+
+function viewBySelectUpdate(){
+    var viewby = $("input[name='viewby']:checked").val();    
+
+    var viewbyOptions = [];
+
+    csvData.map(function(d){return d[viewby]}).forEach(function(d){
+        if(!viewbyOptions.includes(d)){
+            viewbyOptions.push(d);
+        }
+    })
+    $("#viewBySelect").html('');
+    viewbyOptions.forEach(function(opt){
+        var html = '<option value="'+opt+'">'+opt+'</option>';
+        $("#viewBySelect").append(html);
+    })
+}
