2f271adbe51795cabf34f1198db7352179d74e41
Dev <devwork8888@gmail.com>
2020-02-10 22:34:51 +0800

    new data completed except text formatting

--- d3/family-tree/script.js
+++ d3/family-tree/script.js
@@ -5,11 +5,13 @@ var colorArray = {
 
 var marriedCircleWidth = 20, marriedTextFontSize = 12;
 var fontSizeArray = [15,15,14,13,12,11,10,8];
+var marriedFontSizeArray = [12,12,12,10,10,9,9,8];
 var width = 1000,
     height = 1000,
     radius = 50 * Math.max(width, height) / 100,
     x = d3.scaleLinear().range([0, 2 * Math.PI]),
     y = d3.scalePow().exponent(1.3).domain([0, 1]).range([0, radius]),
+    // y = d3.scaleLinear().domain([0,1]).range([0,radius]);
     padding = 5;
 
 var svg = d3.select("#vis")
@@ -94,7 +96,7 @@ var rootobject = 0;
 
 
 function updateGraph(path) {
-    d3.json("data2.json", function(data) {
+    d3.json("fulldata.json", function(data) {
         var tree_data = makeTreeData(data);
         buildGraph(tree_data);
     })
@@ -106,7 +108,8 @@ function buildGraph(jsondata) {
     root.sum(function(d){return d.children ? 0 : 1;});
 
     svg.selectAll("path.married-arc")
-    .data(partition(root).descendants().filter(function(d){return d.depth < 4}))
+    .data(partition(root).descendants().filter(function(d){        
+        return d.depth < root.height;}))
         .enter()
         .append("path")
         .attr('attr-depth',(d)=>d.depth)
@@ -114,11 +117,12 @@ function buildGraph(jsondata) {
         .attr("d", married_arc)
         .style("fill", "#eee");
 
-    var arc_wrapper = svg.selectAll(".arc_wrapper")
-        .data(partition(root).descendants())
-        .enter().append('g').attr('class','arc_wrapper');
+    var inner_arc_wrapper = svg.selectAll(".inner_arc_wrapper")
+        .data(partition(root).descendants().filter(function(d){return d.height > 3;}))
+        // .data(partition(root).descendants())
+        .enter().append('g').attr('class','inner_arc_wrapper');
 
-    arc_wrapper
+    inner_arc_wrapper
         .append("path")
         .attr('class','main-arc')
         .attr("id", function(d, i) {
@@ -138,17 +142,17 @@ function buildGraph(jsondata) {
         .attr("d", arc)
         .attr("fill-rule", "evenodd");
 
-    arc_wrapper.append('path')
+    inner_arc_wrapper.append('path')
         .attr('class', 'hidden-arc')
         .attr('id', (_, i) => `hiddenArc${i}`)
         .attr('d', middleArcLine);
 
-    arc_wrapper.append('path')
+    inner_arc_wrapper.append('path')
         .attr('class', 'hidden-married-arc')
         .attr('id', (_, i) => `hiddenMarriedArc${i}`)
         .attr('d', middleMarriedArcLine);
 
-    const textInfo = arc_wrapper.append('text').style('font-size',d=>{ return fontSizeArray[d.depth]})
+    let textInfo = inner_arc_wrapper.append('text').style('font-size',d=>{ return fontSizeArray[d.depth]})
     // Add white contour
     textInfo.append('textPath')
         .attr('startOffset','50%')
@@ -161,7 +165,7 @@ function buildGraph(jsondata) {
         .attr('xlink:href', (_, i) => `#hiddenArc${i}` )
         .text(d => d.data.name);
 
-    const marriedInfo = arc_wrapper.append('text').attr('class','married-info-text').style('font-size',marriedTextFontSize+'px');
+    let marriedInfo = inner_arc_wrapper.append('text').attr('class','married-info-text').style('font-size',d=>{return marriedFontSizeArray[d.depth]});
     // Add white contour
     marriedInfo.append('textPath')
         .attr('startOffset','50%')
@@ -174,112 +178,173 @@ function buildGraph(jsondata) {
         .attr('xlink:href', (_, i) => `#hiddenMarriedArc${i}` )
         .text(d => d.data.married);
 
+
+    // // Outer 4 circles
+    var outer_arc_wrapper = svg.selectAll(".outer_arc_wrapper")
+        .data(partition(root).descendants().filter(function(d){return d.height <= 3;}))
+        .enter().append('g').attr('class','outer_arc_wrapper');
+
+    outer_arc_wrapper
+        .append("path")
+        .attr('class','main-arc')
+        .attr("id", function(d, i) {
+            if (i == 0) {
+                rootobject = d;
+            }
+            if (d.id == currentid) {
+                currentobject = d;
+            }
+            return "path-" + i;
+        }).style("fill", function(d) {
+            if (d.data.gender == "root") {
+                return "white";
+            }
+            return colorArray[d.data.gender];
+        })
+        .attr("d", arc)
+
+    // outer_arc_wrapper.append('path')
+    //     .attr('class', 'hidden-arc')
+    //     .attr('id', (_, i) => `outerhiddenArc${i}`)
+    //     .attr('d', middleArcLine);
+
+    outer_arc_wrapper.append('path')
+        .attr('class', 'hidden-married-arc')
+        .attr('id', (_, i) => `outerhiddenMarriedArc${i}`)
+        .attr('d', middleMarriedArcLine);
+
+    // outertextInfo = outer_arc_wrapper.append('text').style('font-size',d=>{ return fontSizeArray[d.depth]})
+    // // Add white contour
+    // outertextInfo.append('textPath')
+    //     .attr('startOffset','50%')
+    //     .attr('xlink:href', (_, i) => `#outerhiddenArc${i}` )
+    //     .text(d => d.data.name)
+    //     .style('fill', 'none');
+
+    // outertextInfo.append('textPath')
+    //     .attr('startOffset','50%')
+    //     .attr('xlink:href', (_, i) => `#outerhiddenArc${i}` )
+    //     .text(d => d.data.name);
+
+    outermarriedInfo = outer_arc_wrapper.append('text').attr('class','married-info-text').style('font-size',d=>{return marriedFontSizeArray[d.depth]});
+    // Add white contour
+    // outermarriedInfo.append('textPath')
+    //     .attr('startOffset','50%')
+    //     .attr('xlink:href', (_, i) => `#outerhiddenMarriedArc${i+4}` )
+    //     .text(d=>d.data.married)
+    //     .style('fill', 'none');
+
+    outermarriedInfo.append('textPath')
+        .attr('startOffset','50%')
+        .attr('xlink:href', (_, i) => `#outerhiddenMarriedArc${i}` )
+        .text(d => d.data.married);
+
     // //add text
-    // var text = svg.selectAll("text")
-    //     .data(partition(root).descendants());
+    var text = svg.selectAll("text.out")
+        .data(partition(root).descendants().filter(function(d){return d.height <=3;}));
 
-    // var textEnter = text
-    //     .enter()
-    //     .append("text")
-    //     //starting opacity
-    //     //hides all those but the inner ring
-    //     .style("pointer-events", "none")
-    //     //color fill
-    //     //#000000 is black
-    //     .style("font-size", "10px")
-    //     .style("fill", "#000000")
-    //     .attr("text-anchor", function(d) {
-    //         return x(d.x1 / 2) > Math.PI ? "end" : "start";
-    //     })
+    var textEnter = text
+        .enter()
+        .append("text")
+        .attr('class','out')
+        //starting opacity
+        //hides all those but the inner ring
+        .style("pointer-events", "none")
+        //color fill
+        //#000000 is black
+        .style("font-size", d=>{return fontSizeArray[d.depth]})
+        .style("fill", "#000000")
+        .attr("text-anchor", function(d) {
+            return x(d.x1 / 2) > Math.PI ? "end" : "start";
+        })
 
-    //     .attr("dy", ".2em")
-    //     //checks for multiline names
-    //     .attr("transform", function(d) {
-    //         var multiline = (d.data.name || "")
-    //             .split(" ")
-    //             .length > 1.5,
-    //             angle = x(d.x1 / 2) * 180 / Math.PI - 90,
-    //             rotate = angle + (multiline ? -.5 : 0);
+        .attr("dy", ".2em")
+        //checks for multiline names
+        .attr("transform", function(d) {
+            var multiline = (d.data.name || "")
+                .split(" ")
+                .length > 1.5,
+                // angle = x(d.x + d.dx / 2) * 180 / Math.PI - 90,
+                angle = x(d.x0 + (d.x1 - d.x0) / 2) * 180 / Math.PI - 90,
+                rotate = angle + (multiline ? -.5 : 0);
 
-    //         return "rotate(" + rotate + ")translate(" + (y(d.y0) + marriedCircleWidth + padding) + ")rotate(" + (angle > 90 ? -180 : 0) + ")";
-    //     })
+                console.log(angle);
+            return "rotate(" + rotate + ")translate(" + (y(d.y0) + 2*marriedCircleWidth + padding) + ")rotate(" + (angle > 90 ? -180 : 0) + ")";
+        })
 
-    // //1st row of text
-    // textEnter
-    //     .append("tspan")
-    //     .attr("x", 0)
-    //     .text(function(d) {
-    //         txt = d.data.name.split(" ");
-    //         for (var i = txt.length - 1; i >= 0; i--) {
-    //             if (txt[i].length < 4 && i != 0) {
-    //                 txt[i - 1] += txt[i];
-    //                 txt.splice(i, 1);
-    //             }
+    //1st row of text
+    textEnter
+        .append("tspan")
+        .attr("x", 0)
+        .text(function(d) {
+            txt = d.data.name.split(" ");
+            for (var i = txt.length - 1; i >= 0; i--) {
+                if (txt[i].length < 4 && i != 0) {
+                    txt[i - 1] += txt[i];
+                    txt.splice(i, 1);
+                }
 
-    //         }
-    //         return d.depth ? txt[0] : "";
-    //     });
+            }
+            return d.depth ? txt[0] : "";
+        });
 
-    // //2nd row of text
-    // textEnter
-    //     .append("tspan")
-    //     .attr("x", 0)
-    //     .attr("dy", ".9em")
-    //     .text(function(d) {
-    //         txt = d.data.name.split(" ");
-    //         for (var i = txt.length - 1; i >= 0; i--) {
-    //             if (txt[i].length < 4 && i != 0) {
-    //                 txt[i - 1] += txt[i];
-    //                 txt.splice(i, 1);
-    //             }
+    //2nd row of text
+    textEnter
+        .append("tspan")
+        .attr("x", 0)
+        .attr("dy", ".9em")
+        .text(function(d) {
+            txt = d.data.name.split(" ");
+            for (var i = txt.length - 1; i >= 0; i--) {
+                if (txt[i].length < 4 && i != 0) {
+                    txt[i - 1] += txt[i];
+                    txt.splice(i, 1);
+                }
 
-    //         }
-    //         return d.depth ? txt[1] || "" : "";
-    //     });
+            }
+            return d.depth ? txt[1] || "" : "";
+        });
 
-    // //3rd row
-    // textEnter
-    //     .append("tspan")
-    //     .attr("x", 0)
-    //     .attr("dy", ".9em")
-    //     .text(function(d) {
-    //         txt = d.data.name.split(" ");
-    //         for (var i = txt.length - 1; i >= 0; i--) {
-    //             if (txt[i].length < 4 && i != 0) {
-    //                 txt[i - 1] += txt[i];
-    //                 txt.splice(i, 1);
-    //             }
+    //3rd row
+    textEnter
+        .append("tspan")
+        .attr("x", 0)
+        .attr("dy", ".9em")
+        .text(function(d) {
+            txt = d.data.name.split(" ");
+            for (var i = txt.length - 1; i >= 0; i--) {
+                if (txt[i].length < 4 && i != 0) {
+                    txt[i - 1] += txt[i];
+                    txt.splice(i, 1);
+                }
 
-    //         }
-    //         return d.depth ? txt[2] || "" : "";
-    //     });
+            }
+            return d.depth ? txt[2] || "" : "";
+        });
 
-    // //fourth row (if necessary)
-    // textEnter
-    //     .append("tspan")
-    //     .attr("x", 0)
-    //     .attr("dy", ".9em")
-    //     .text(function(d) {
-    //         txt = d.data.name.split(" ");
-    //         for (var i = txt.length - 1; i >= 0; i--) {
-    //             if (txt[i].length < 4 && i != 0) {
-    //                 txt[i - 1] += txt[i];
-    //                 txt.splice(i, 1);
-    //             }
+    //fourth row (if necessary)
+    textEnter
+        .append("tspan")
+        .attr("x", 0)
+        .attr("dy", ".9em")
+        .text(function(d) {
+            txt = d.data.name.split(" ");
+            for (var i = txt.length - 1; i >= 0; i--) {
+                if (txt[i].length < 4 && i != 0) {
+                    txt[i - 1] += txt[i];
+                    txt.splice(i, 1);
+                }
 
-    //         }
-    //         return d.depth ? txt[3] || "" : "";
-    //     });
+            }
+            return d.depth ? txt[3] || "" : "";
+        });
 
     // var g_wrapper = svg.selectAll('.g_wrapper').data(partition.nodes(root)).enter().append('g').attr('class','g_wrapper');
-
-    // d3.selectAll("path")
-    //     .attr("fill-rule", "evenodd")
-
+    
 }
 
 window.onload = function() {
-    updateGraph("index2.json");
+    updateGraph();
 }
 
 function makeTreeData(data) {
@@ -293,7 +358,7 @@ function insertData(data, gender = "root") {
     tmp.name = data.first + " " + data.last;
     tmp.birth = data.birth;
     tmp.gender = gender;
-    if (Object.keys(data.parents).length != 0) {
+    if(Object.keys(data).includes('parents')){    
         tmp.children = [];
         tmp.children.push(insertData(data.parents.mother, "mother"));
         tmp.children.push(insertData(data.parents.father, "father"));
