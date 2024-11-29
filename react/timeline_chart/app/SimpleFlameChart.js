5da699800f13763d5afb99b74dfeffe45de4aadc
bkirilov <boris.nsdev90@gmail.com>
2024-11-29 23:17:50 +0200

    security fix

--- react/timeline_chart/app/SimpleFlameChart.js
+++
@@ -1,7 +0,0 @@
-import { renderSimpleFlameChart, unrenderSimpleFlameChart, injectSimpleFlameChartStyles } from '../src';
-import * as HSLColorGenerator from '../src/helpers/HSLColorGenerator';
-
-global.HSLColorGenerator = HSLColorGenerator;
-global.renderSimpleFlameChart = renderSimpleFlameChart;
-global.unrenderSimpleFlameChart = unrenderSimpleFlameChart;
-global.injectSimpleFlameChartStyles = injectSimpleFlameChartStyles;
